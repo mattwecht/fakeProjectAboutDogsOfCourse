8cf821720fa186a7f353e6edf8c8f7c49ee2a9bc
Matt Wecht <mattwecht@gmail.com>
2023-06-27 09:30:05 -0400

    Initial Commit

---
+++ fakeProjectAboutDogsOfCourse/fakeProjectAboutDogsOfCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  fakeProjectAboutDogsOfCourseApp.swift
+//  fakeProjectAboutDogsOfCourse
+//
+//  Created by scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct fakeProjectAboutDogsOfCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
